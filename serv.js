46b12de6c79217c93b59b9baa095a47987c6b291
Michael George <mdgeorge@cs.cornell.edu>
2016-03-17 02:44:35 -0400

    app now computing the right thing; need to deal with rate limiting, etc.

--- serv.js
+++ serv.js
@@ -1,19 +1,61 @@
 var Http    = require('http');
 var Promise = require('promise');
-var Ty      = require('then-yield').using(Promise.resolve);
+var Ty      = require('then-yield');
 var Lol     = require('lol-js').client({
   apiKey: fs.readFileSync('api.key').slice(0,-1),
 });
 
 
+var getParticipant = function (match, summId) {
+  pId = match.participantIdentities.find (function (participantIdentity) {
+    return participantIdentity.player.summonerId = summId;
+  });
 
+  participant = match.participants.find (function (participant) {
+    return participant.participantId = pId.participantId;
+  });
+
+  return participant;
+}
+
+/**
+ * returns a 7x24 array results[day][hour].  Each entry has a .wins and a
+ * .losses integer.
+ */
 var getStats = Ty.async(function* (region, summName) {
   var summList = yield Lol.getSummonersByName('na', [summName]);
-  var summ     = summList[summName];
-  var id       = summ.id;
-  var matches  = yield Lol.getMatchlistBySummoner(region, id);
+  var summId   = summList[summName].id;
+  var matches  = yield Lol.getMatchlistBySummoner(region, summId);
 
-  return matches;
+  /* initialize results[day][hour].wins = 0 and .losses = 0 */
+  var results = new Array (7);
+  for (var day = 0; day < 7; day++) {
+    results[day] = new Array (24);
+    for (var hour = 0; hour < 24; hour++) {
+      results[day][hour] = {wins: 0, losses: 0};
+    }
+  }
+
+  for (var i in matches.matches) {
+    var match = yield Lol.getMatch(region, matches.matches[i].matchId);
+
+    var timestamp = new Date(match.matchCreation);
+    var day       = timestamp.getDay();
+    var hour      = timestamp.getHours();
+
+    var entry = results[day][hour];
+
+    var participant = getParticipant(match, summId);
+    if (participant.stats.winner)
+      entry.wins ++;
+    else
+      entry.losses ++;
+
+    if (i % 10 == 0)
+      console.log(i + "/" + matches.matches.length);
+  }
+
+  return results;
 });
 
 var knot = getStats('na','KnotOfGordium');
