35e5231fe02372104ddf9537ebe1e43a0ad6d783
Michael George <mdgeorge@cs.cornell.edu>
2016-03-19 18:12:37 -0400

    made getStats do less aggregation; returns a list of all matches now

--- serv.js
+++ serv.js
@@ -5,11 +5,11 @@ var LolLib  = require('lol-js');
 
 var Lol     = LolLib.client({
   apiKey: fs.readFileSync('api.key').slice(0,-1),
-  cache:  LolLib.redisCache({host: '127.0.0.1', port: 6380})
+  cache:  LolLib.redisCache({host: '127.0.0.1', port: 6380}),
   rateLimit: [
     {time: 10,  limit: 8},
     {time: 600, limit: 400},
-  ]
+  ],
 });
 
 
@@ -25,46 +25,26 @@ var getParticipant = function (match, summId) {
   return participant;
 }
 
-/**
- * returns a 7x24 array results[day][hour].  Each entry has a .wins and a
- * .losses integer.
- */
 var getStats = Ty.async(function* (region, summName) {
   var summList = yield Lol.getSummonersByName('na', [summName]);
   var summId   = summList[summName].id;
   var matches  = yield Lol.getMatchlistBySummoner(region, summId);
 
-  /* initialize results[day][hour].wins = 0 and .losses = 0 */
-  var results = new Array (7);
-  for (var day = 0; day < 7; day++) {
-    results[day] = new Array (24);
-    for (var hour = 0; hour < 24; hour++) {
-      results[day][hour] = {wins: 0, losses: 0};
-    }
-  }
+  var results = new Array(matches.matches.length);
 
-  var resultsCount = 0;
   for (var i in matches.matches) {
-    var match = yield Lol.getMatch(region, matches.matches[i].matchId);
+    var match = matches.matches[i];
+    var matchDetail = yield Lol.getMatch(region, match.matchId);
 
-    var timestamp = new Date(match.matchCreation);
-    var day       = timestamp.getDay();
-    var hour      = timestamp.getHours();
-
-    var entry = results[day][hour];
-
-    var participant = getParticipant(match, summId);
-    if (participant.stats.winner)
-      entry.wins ++;
-    else
-      entry.losses ++;
-
-    resultsCount++;
-
-    if (i % 10 == 0)
-      console.log(i + "/" + matches.matches.length + " (" + resultsCount + ")");
-    if (i % 30 == 0)
-      console.log(results);
+    results[i] = {
+      id:        match.matchId,
+      champion:  match.champion,
+      queue:     match.queue,
+      lane:      match.lane,
+      role:      match.role,
+      timestamp: match.timestamp,
+      winner:    getParticipant(matchDetail, summId).stats.winner,
+    };
   }
 
   return results;
