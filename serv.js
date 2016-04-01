5d7271462e0a04c14acdc8f201b4625b4702e6ca
Michael George <mdgeorge@cs.cornell.edu>
2016-04-01 13:41:49 -0400

    tabs

--- serv.js
+++ serv.js
@@ -3,67 +3,71 @@ var Promise = require('promise');
 var Ty      = require('then-yield');
 var LolLib  = require('lol-js');
 
-var Lol     = LolLib.client({
-  apiKey: fs.readFileSync('api.key').slice(0,-1),
-  cache:  LolLib.redisCache({host: '127.0.0.1', port: 6380}),
-  rateLimit: [
-    {time: 10,  limit: 8},
-    {time: 600, limit: 400},
-  ],
+var Lol = LolLib.client({
+	apiKey: fs.readFileSync('api.key').slice(0,-1),
+	cache:  LolLib.redisCache({host: '127.0.0.1', port: 6380}),
+	rateLimit: [
+		{time: 10,  limit: 8},
+		{time: 600, limit: 400},
+	],
 });
 
 
 var getParticipant = function (match, summId) {
-  pId = match.participantIdentities.find (function (participantIdentity) {
-    return participantIdentity.player.summonerId = summId;
-  });
+	pId = match.participantIdentities.find (function (participantIdentity) {
+		return participantIdentity.player.summonerId = summId;
+	});
 
-  participant = match.participants.find (function (participant) {
-    return participant.participantId = pId.participantId;
-  });
+	participant = match.participants.find (function (participant) {
+		return participant.participantId = pId.participantId;
+	});
 
-  return participant;
+	return participant;
 }
 
 var getStats = Ty.async(function* (region, summName) {
-  var summList = yield Lol.getSummonersByName('na', [summName]);
-  var summId   = summList[summName].id;
-  var matches  = yield Lol.getMatchlistBySummoner(region, summId);
+	var summList = yield Lol.getSummonersByName('na', [summName]);
+	var summId	 = summList[summName].id;
+	var matches  = yield Lol.getMatchlistBySummoner(region, summId);
 
-  var results = new Array(matches.matches.length);
+	var results = new Array(matches.matches.length);
 
-  for (var i in matches.matches) {
-    var match = matches.matches[i];
-    var matchDetail = yield Lol.getMatch(region, match.matchId);
+	for (var i in matches.matches) {
+		var match = matches.matches[i];
+		var matchDetail = yield Lol.getMatch(region, match.matchId);
 
-    results[i] = {
-      id:        match.matchId,
-      champion:  match.champion,
-      queue:     match.queue,
-      lane:      match.lane,
-      role:      match.role,
-      timestamp: match.timestamp,
-      winner:    getParticipant(matchDetail, summId).stats.winner,
-    };
-  }
+		results[i] = {
+			id:        match.matchId,
+			champion:  match.champion,
+			queue:     match.queue,
+			lane:      match.lane,
+			role:      match.role,
+			timestamp: match.timestamp,
+			winner:    getParticipant(matchDetail, summId).stats.winner,
+		};
+	}
 
-  return results;
+	return results;
 });
 
 var knot = getStats('na','KnotOfGordium');
  
 knot.then(function (stats) {
-  fs.writeFileSync('ui/data.json', JSON.stringify(stats, null, 2));
-  console.log(stats);
+	fs.writeFileSync('ui/data.json', JSON.stringify(stats, null, 2));
+	console.log(stats);
 });
 
 /*
 
 var server = http.createServer(function (req, resp) {
-  resp.writeHead(200);
-  resp.end('Hello world')
+	resp.writeHead(200);
+	resp.end('Hello world')
 });
 
 server.listen(8080)
+*/
+
+/*
+** vim: ts=4 sw=4
 */
 
